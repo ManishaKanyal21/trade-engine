524425b58e69f2bd887e7fb82640db0456f25166
ManishaKanyal21 <manishajobs21@gmail.com>
2025-01-07 18:00:15 +0000

    Trade report generation logic

---
+++ src/main/TradeInstruction.java
@@ -0,0 +1,101 @@
+package main;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import static main.TradeReportUtility.dateFormatter;
+
+/**
+ * Class that holds trade Instruction
+ */
+public final class TradeInstruction {
+
+    private final String entityName;
+    private final InstructionType instructionType;
+    private final double agreedFx;
+    private final LocalDate instructionDate;
+    private final String currency;
+    private final int numberOfUnits;
+    private final double pricePerUnit;
+    private final LocalDate actualSettlementDate;
+
+    /**
+     * @param entityName               name of the entity
+     * @param instructionType          "B" or "S"
+     * @param agreedFx                 exchange rate
+     * @param currency                 name of the currency
+     * @param instructionDate          date when the transaction was instructed
+     * @param instructedSettlementDate date when transaction is instructed to be settled
+     * @param numberOfUnits            number of units to buy or sell
+     * @param pricePerUnit             price of a unit
+     */
+    public TradeInstruction(final String entityName, final String instructionType, final double agreedFx,
+                            final String currency, final String instructionDate,
+                            final String instructedSettlementDate,
+                            final int numberOfUnits, final double pricePerUnit) {
+        Objects.requireNonNull(entityName, "Entity name should be provided.");
+        Objects.requireNonNull(entityName, "Instruction type should be provided.");
+        Objects.requireNonNull(entityName, "Currency should be provided.");
+        Objects.requireNonNull(entityName, "Settlement date should be provided.");
+
+        this.entityName = entityName;
+        this.instructionType = InstructionType.fromType(instructionType);
+        this.agreedFx = agreedFx;
+        this.instructionDate = LocalDate.parse(instructionDate, dateFormatter);
+        this.numberOfUnits = numberOfUnits;
+        this.pricePerUnit = pricePerUnit;
+        this.currency = currency;
+        this.actualSettlementDate = calculateActualSettlementDate(instructedSettlementDate, currency);
+    }
+
+    /**
+     * This method calculates the working day when settlement can take place based on the instructed settlement date and currency.
+     *
+     * @param instructedSettlementDate - date instructed by the client for settlement of trade.
+     * @param currency                 - currency of the trade
+     * @return actual date when settlement will take place.
+     */
+    private LocalDate calculateActualSettlementDate(final String instructedSettlementDate, final String currency) {
+        final LocalDate settlementDate = LocalDate.parse(instructedSettlementDate, dateFormatter);
+        final DayOfWeek dayOfWeek = settlementDate.getDayOfWeek();
+        if ("AED".equalsIgnoreCase(currency) || "SAR".equalsIgnoreCase(currency)) {
+            if (DayOfWeek.FRIDAY.equals(dayOfWeek)) {
+                return settlementDate.plusDays(2);
+            } else if (DayOfWeek.SATURDAY.equals(dayOfWeek)) {
+                return settlementDate.plusDays(1);
+            }
+        } else {
+            if (DayOfWeek.SATURDAY.equals(dayOfWeek)) {
+                return settlementDate.plusDays(2);
+            } else if (DayOfWeek.SUNDAY.equals(dayOfWeek)) {
+                return settlementDate.plusDays(1);
+            }
+        }
+        return settlementDate;
+    }
+
+    String getEntityName() {
+        return entityName;
+    }
+
+    InstructionType getInstructionType() {
+        return instructionType;
+    }
+
+    LocalDate getActualSettlementDate() {
+        return actualSettlementDate;
+    }
+
+    LocalDate getInstructionDate() {
+        return instructionDate;
+    }
+
+    String getCurrency() {
+        return currency;
+    }
+
+    double calculateUSDAmount() {
+        return pricePerUnit * numberOfUnits * agreedFx;
+    }
+}
---
+++ src/main/TradeReport.java
@@ -0,0 +1,58 @@
+package main;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import static main.TradeReportUtility.dateFormatter;
+
+/**
+ * This class generates trade report.
+ */
+public class TradeReport {
+
+    public static void main(String[] args) {
+
+        final List<TradeInstruction> tradeDetails = TradeReportUtility.getTradeInstructions();
+
+        final Map<LocalDate, Double> outgoingSettlements = TradeReportUtility.getUSDSettlements(tradeDetails, InstructionType.BUY);
+
+        final Map<LocalDate, Double> incomingSettlements = TradeReportUtility.getUSDSettlements(tradeDetails, InstructionType.SELL);
+
+        final List<Map.Entry<String, Double>> outgoingRankings = TradeReportUtility.getRankings(tradeDetails, InstructionType.BUY);
+
+        final List<Map.Entry<String, Double>> incomingRankings = TradeReportUtility.getRankings(tradeDetails, InstructionType.SELL);
+
+        System.out.println("### Outgoing USD Settlements for dates ###");
+        for (Map.Entry<LocalDate, Double> entry : outgoingSettlements.entrySet()) {
+            final String date = entry.getKey().format(dateFormatter);
+            final double amount = entry.getValue();
+            System.out.println(String.format("%s - $(%.2f)", date, amount));
+        }
+        System.out.println("### End of Outgoing USD Settlements ###");
+        System.out.println(" ");
+        System.out.println("### Incoming USD Settlements for dates ###");
+        for (Map.Entry<LocalDate, Double> entry : incomingSettlements.entrySet()) {
+            final String date = entry.getKey().format(dateFormatter);
+            final double amount = entry.getValue();
+            System.out.println(String.format("%s - $(%.2f)", date, amount));
+        }
+        System.out.println("### End of Incoming USD Settlements ###");
+        System.out.println("         ");
+        System.out.println("### Ranking of Entities based on Outgoing USD Settlements ### ");
+        int ranking = 1;
+        for(Map.Entry<String,Double> entry : outgoingRankings) {
+            System.out.println(String.format("%d) %s $(%.2f)", ranking ++, entry.getKey(), entry.getValue()));
+        }
+        System.out.println("### End of Ranking of Outgoing USD Settlements ###            ");
+
+        System.out.println("         ");
+        System.out.println("### Ranking of Entities based on Incoming USD Settlements ### ");
+        ranking = 1;
+        for(Map.Entry<String,Double> entry : incomingRankings) {
+            System.out.println(String.format("%d) %s $(%.2f)", ranking ++, entry.getKey(), entry.getValue()));
+        }
+        System.out.println("### End of Ranking of Incoming USD Settlements ### ");
+    }
+
+}
